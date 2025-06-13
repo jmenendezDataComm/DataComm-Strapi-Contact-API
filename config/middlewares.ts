2cfc787c75ae8a25975142a1b93a3fb744eb94f8
James Menendez <jamesmenendez@datacomm.com>
2025-06-12 20:45:30 -0400

    update for ckeditor middleware error.

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -16,7 +16,7 @@ export default [
         cors: { enabled: true, headers: '*' },
         useDefaults: true,
         directives: {
-          'script-src': ['https://cdn.ckeditor.com'],
+          'script-src': ["'self'", "'unsafe-inline'",'https://cdn.ckeditor.com'],
           'connect-src': ['https://proxy-event.ckeditor.com'],
           "frame-src":[
                         "'self'",
